e4c1b79e963e444c872a812abbf45c77bda7d18e
Felix Rigau <felixrigau88@gmail.com>
2018-02-05 17:58:03 +0000

    Adding tools.js file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+var myApp = myApp || {};
+
+var url = `https://api.edamam.com/search?q=chicken&app_id=${myApp.config.appId}&app_key=${myApp.config.appKey}&from=0&to=1`;
+ 
 document.querySelector('.general-container').addEventListener('click',function () {
-    window.alert('hello');
+    myApp.tools.makeAjaxRequest('GET', '../localDatas/recipes.json', true, function (data) {
+        console.log(data);
+    });
 })
