0537d0f0ece3cfc69d2ba9b5f99a4a8d7774f8be
Felix Rigau <felixrigau88@gmail.com>
2018-02-05 18:08:24 +0000

    Removing unnecesary files

--- js/app.js
+++ js/app.js
@@ -1,9 +1,4 @@
 var myApp = myApp || {};
 
-var url = `https://api.edamam.com/search?q=chicken&app_id=${myApp.config.appId}&app_key=${myApp.config.appKey}&from=0&to=1`;
- 
-document.querySelector('.general-container').addEventListener('click',function () {
-    myApp.tools.makeAjaxRequest('GET', '../localDatas/recipes.json', true, function (data) {
-        console.log(data);
-    });
-})
+// var url = `https://api.edamam.com/search?q=chicken&app_id=${myApp.config.appId}&app_key=${myApp.config.appKey}&from=0&to=1`;
+var url = '../localDatas/recipes.json';
